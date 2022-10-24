1e8e9a0d5762978422e2a76ada73a6c72ccd4d61
yinozemcev <88054444+yinozemcev@users.noreply.github.com>
2022-10-24 11:57:51 +0300

    Add files via upload

--- graph/friends_parser.py
+++ graph/friends_parser.py
@@ -1,6 +1,7 @@
 import vk_api
 from time import sleep
-import json
+import pickle
+from datetime import datetime
 
 """
 Файл private.txt должен иметь следующее содержание:
@@ -20,34 +21,34 @@ vk_session.auth()
 vk = vk_session.get_api()
 
 edges = []
+first_friends = []
 for user_id in IDs:
     try:
         friends = vk.friends.get(user_id = user_id)['items']
         for friend in friends:
-            if friend not in IDs:
-                edges.append([int(user_id), friend])
+            first_friends.append(friend)
+            edges.append((int(user_id), friend))
         sleep(0.35)
     except Exception as e:
         with open('logs.txt', 'a') as logs:
-            logs.write(f'Ошибка с профилем {user_id}: {e}\n')
+            logs.write(f'[{datetime.now()}] Ошибка с профилем {user_id}: {e}\n')
 """
-first_friends = set(edge[1] for edge in edges)
-
+first_friends = set(first_friends)
 print(len(first_friends))
 
 for user_id in first_friends:
     try:
         second_friends = vk.friends.get(user_id = user_id)['items']
         for friend in second_friends:
-            if friend not in first_friends:
-                edges.append([int(user_id), friend])
+            edges.append((int(user_id), friend))
         sleep(0.35)
     except Exception as e:
         with open('logs.txt', 'a') as logs:
-            logs.write(f'Ошибка с профилем {user_id}: {e}\n')
+            logs.write(f'[{datetime.now()}] Ошибка с профилем {user_id}: {e}\n')
 """
-data = json.dumps(edges)
-print(len(edges))
-with open('result.txt', 'w') as file:
-    file.write(data)
+total_edges = set(tuple(sorted(edge)) for edge in edges)
+print(len(total_edges))
+timestamp = datetime.now().strftime('%d_%m_%Y__%H_%M_%S')
+with open(f'result_{timestamp}.pickle', 'wb') as file:
+    pickle.dump(total_edges, file)
 
--- graph/graph.py
+++ graph/graph.py
@@ -1,19 +1,23 @@
-import json
+import pickle
 import networkx
+import matplotlib.pyplot as plt
 
-with open('result.txt') as file:
-    edges = json.loads(file.read())
+filename = input('Введите название файла с рёбрами графа: ')
+with open(filename, 'rb') as file:
+    edges = pickle.load(file)
 
 G = networkx.Graph()
 G.add_edges_from(edges)
-networkx.write_gexf(G, 'result.gexf')
+networkx.write_gexf(G, filename[:-7] + '.gexf')
 
-posrednichestvo = networkx.betweenness_centrality(G)
-blizost = networkx.closeness_centrality(G)
-sobstvenniy = networkx.eigenvector_centrality(G)
+betweenness = networkx.betweenness_centrality(G)
+closeness = networkx.closeness_centrality(G)
+eigenvector = networkx.eigenvector_centrality(G)
 
-user_id = int(input('Введите ID пользователя для которого нужна центральность: '))
-print(f'Центральность по посредничеству: {posrednichestvo[user_id]}')
-print(f'Центральность по близости: {blizost[user_id]}')
-print(f'Центральность по собственному значению: {sobstvenniy[user_id]}')
+max_betweenness = max(betweenness, key=betweenness.get)
+max_closeness = max(closeness, key=closeness.get)
+max_eigenvector = max(eigenvector, key=eigenvector.get)
+print(f'Самый центральный по посредничеству: {max_betweenness}')
+print(f'Самый центральный по близости: {max_closeness}')
+print(f'Самый центральный по собственному значению: {max_eigenvector}')
 
